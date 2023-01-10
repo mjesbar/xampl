a891c1bcfc4f0593bfa15f545780b48b8cc8d067
niggue <miguedjba96@gmail.com>
2023-01-10 11:10:06 -0500

    starting aws folder

--- aws/aws_s3_list.py
+++ aws/aws_s3_list.py
@@ -2,9 +2,12 @@ import boto3
 import json
 
 
+# pretty function prints as beauty the s3 response
 def pretty(object):
+
     pretty_json_to_print = json.dumps(object, indent=4, default=str)
     return pretty_json_to_print
+
 
 
 s3_client = boto3.client("s3")
