223ac1b1408f6cbe983c38ee2f85b37103df909a
niggue <miguedjba96@gmail.com>
2023-03-13 16:35:48 -0500

    ~

--- python/spark.py
+++ python/spark.py
@@ -1,5 +1,4 @@
 from pyspark.sql import SparkSession
-import pyspark.implicits._
 import json
 
 
@@ -8,10 +7,21 @@ spark = SparkSession.builder \
     .config("spark.sparkContext.setLogLevel('INFO')") \
     .getOrCreate()
 
-file = {
-    "name": "John",
-    "age": 30
-}
+file = \
+[
+    {
+        "name": "John",
+        "age": 30
+    },
+    {
+        "name": "Adriana",
+        "age": 25
+    },
+    {
+        "name": "Lucio",
+        "age": 56
+    }
+]
 
 file = json.dumps(file, indent=4)
 
@@ -21,11 +31,28 @@ df = spark.read.json("file:///home/debian/mega/github/xampl/python/test.json",
                      multiLine=True,
                      encoding="utf-8")
 
+print("Schema:")
 df.printSchema()
-df .show()
 
-df.select("name", "age").show()
-print(type(df))
+print("DataFrame:")
+df.show()
 
+print("Reordering DataFrame:")
+df = df.select("name", "age")
+df.show()
 
+print("People over 30 years old:")
+df.filter(df["age"] >= 30).show()
 
+print("People below 30 years old:")
+df.filter(df["age"] <= 30).show()
+
+print("Quering by TempView:")
+sqlDF = df.createOrReplaceTempView("fellows")
+sqlDF = spark.sql("SELECT * FROM fellows WHERE age < 30")
+sqlDF.show()
+
+print("Quering by GlobalTempView 'global_temp' database:")
+sqlDF = df.createGlobalTempView("fellows")
+sqlDF = spark.sql("SELECT * FROM global_temp.fellows WHERE name = 'John'")
+sqlDF.show()
