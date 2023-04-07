3b15ca65a04db1ba0a0f88f0f2f3376d4a90ed08
mjesbar <miguedjba96@gmail.com>
2023-04-07 16:11:30 -0500

    ~

--- fullstack/django/mysite/apphome/views.py
+++ fullstack/django/mysite/apphome/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+
 
 # Create your views here.
-
+# 
 def home(request):
     """
     Return the homepage."""
+
     return render(request, 'home.html')
    
+
 
 def room(request):
     """
@@ -20,15 +22,18 @@ def room(request):
 def variable(request):
     """
     Return the shared_variables page."""
+
     var = 'This is a variable : String'
     vars = ['var1', 'var2', 'varN', '. . .']
     context = {'data': var, 'datas': vars}
+
     return render(request, 'variable.html', context)
 
 
 def extend(request):
     """
     Return the extend-html page."""
+
     return render(request, 'extend.html')
 
 
