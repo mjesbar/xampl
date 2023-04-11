fd9ab3835afb8d860baf769e2e11593fd48baf32
mjesbar <miguedjba96@gmail.com>
2023-04-10 19:05:34 -0500

    setting admin django

--- fullstack/django/mysite/siteconf/settings.py
+++ fullstack/django/mysite/siteconf/settings.py
@@ -113,6 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+#  LANGUAGE_CODE = 'es-co'
 
 #  TIME_ZONE = 'UTC'
 TIME_ZONE = 'America/Bogota'
