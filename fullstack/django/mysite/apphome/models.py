015ed758404f4fad2ca7751d3a00e3e6a85ca0e5
mjesbar <miguedjba96@gmail.com>
2023-04-18 10:24:51 -0500

    ~

--- fullstack/django/mysite/apphome/models.py
+++ fullstack/django/mysite/apphome/models.py
@@ -4,31 +4,24 @@ from django.db import models
 
 
 class Question(models.Model):
-    #
-    """ Question Model """
+    """Question Model."""
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    visits = models.IntegerField(default=0)
 
     def __str__(self): 
-        #
         return self.question_text
-        #
-
     #Question end
 
 
 class Choice(models.Model):
-    #
-    """ Choice Model """
+    """Choice Model."""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self): 
-        #
         return self.choice_text
-        #
-
     #Choice end
 
 
--- fullstack/django/mysite/apphome/urls.py
+++ fullstack/django/mysite/apphome/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+
 from . import views
 
+app_name = "apphome"
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('room/', views.room, name='room'),
-    path('variable/', views.variable, name='variable'),
-    path('extend/', views.extend, name='extend'),
-    path('view/<int:question_id>/', views.view, name = 'view'),
+    path("", views.home, name="home"),
+    path("room/", views.room, name="room"),
+    path("variable/", views.variable, name="variable"),
+    path("extend/", views.extend, name="extend"),
+    path("view/<int:question_id>/", views.view, name="view"),
 ]
