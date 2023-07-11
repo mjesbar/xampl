be7515c003c214433dab891ebdee8d2a8252661b
mjesbar <miguedjba96@gmail.com>
2023-07-11 17:44:26 -0500

    class learning in c#. first commit

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Net;
+using System.Net.Http;
+
+
+
+public class Vector2D
+{
+	// attributes
+	protected int x;
+	protected int y;
+	
+	public Vector2D(int p_x, int p_y)
+	{
+		x = p_x;
+		y = p_y;
+	}
+
+	// methods
+	public string printVariables()
+	{
+		return $"{x} and {y}";
+	}
+}
+
+public class Vector3D : Vector2D
+{
+	public int z;
+
+	public Vector3D(int p_x, int p_y, int p_z) : base(p_x, p_y)
+	{
+		z = p_z;
+	}
+
+	public new string printVariables()
+	{
+		return $"{x}, {y}, and {z}";
+	}
+}
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Vector3D testVector = new Vector3D(10, 34, 10);
+		Vector3D nanoVector = new Vector3D(5, 6, 7);	
+
+		Console.WriteLine("testVector: " + testVector.printVariables());
+		Console.Write("nanoVector: " + nanoVector.printVariables());
+	}
+}
+
